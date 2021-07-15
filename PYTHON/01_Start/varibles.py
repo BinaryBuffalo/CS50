d664250ddbc1e41d636f34017cbae59d971f9ca1
root <root@DESKTOP-294OK59.localdomain>
2021-07-15 12:34:41 -0600

    Python Start

---
+++ PYTHON/01_Start/varibles.py
@@ -0,0 +1,5 @@
+a = 28 #int
+b = 1.5 #Float
+c = "Hello" #String
+d = True #BOOl
+e = '\0' #None
---
+++ PYTHON/02_Conditions/conditions.py
@@ -0,0 +1,7 @@
+n = int(input("Number -> "))
+if n > 0:
+    print("You've enterd a positive number")
+elif n < 0:
+    print("You've enterd a negative number")
+else:
+    print("Input is ( 0 ) ")
