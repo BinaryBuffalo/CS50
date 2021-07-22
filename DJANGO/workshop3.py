5dd1423b70ebab69d8a00143866410b0b8c3a89c
root <root@DESKTOP-294OK59.localdomain>
2021-07-22 01:23:44 -0600

    Django Work

---
+++ DJANGO/workshop3.py
@@ -0,0 +1,15 @@
+import os
+import time
+print("Hello, o_o -> :)\n")
+time.sleep(2.22)
+print("installing python pip3 | DEBIAN")
+cmd = 'chmod +x workshop.py3;sudo apt-get install python3-pip'
+os.system(cmd)
+print("\nRun Me    => /root\n")
+time.sleep(3.69)
+print("Installing Django | DEBIAN")
+cmd2 = 'pip3 install Django'
+time.sleep(3.33)
+print("Complete")
+print("Bye\n")
+os.system(cmd2)
